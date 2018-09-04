562c33285bf13783a9a120fc19e383bf63cdeb39
Rachit Anand <rachitanand@ip-63-107.wvu.edu>
2018-09-04 15:03:35 -0400

    Added events for new messages

--- public/js/index.js
+++ public/js/index.js
@@ -3,10 +3,6 @@ let socket = io();
 socket.on('connect', function () {
     console.log('Connected to server');
 
-    socket.emit('createMessage', {
-        to: 'jen',
-        text: 'Hey! This is Rachit!'
-    })
 });
 
 socket.on('newMessage', function (message)  {
