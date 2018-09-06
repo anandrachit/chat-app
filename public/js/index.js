40272b19723b7584864c0bbdc9a41ba0dcc3b8e8
Rachit Anand <rachitanand@Rachits-MacBook-Pro.local>
2018-09-05 21:08:12 -0400

    Scroll to bottom if user is close to bottom

--- public/js/index.js
+++ public/js/index.js
@@ -1,5 +1,21 @@
 let socket = io();
 
+function scrollToBottom() {
+    //Selectors
+    let messages = jQuery('#messages');
+    let newMessage = messages.children('li:last-child');
+    //Heights
+    let clientHeight = messages.prop('clientHeight');
+    let scrollTop = messages.prop('scrollTop');
+    let scrollHeight = messages.prop('scrollHeight');
+    let newMessageHeight = newMessage.innerHeight();
+    let lastMessageHeight = newMessage.prev().innerHeight();
+
+    if (clientHeight + scrollTop + newMessageHeight + lastMessageHeight >= scrollHeight){
+        messages.scrollTop(scrollHeight);
+    }
+}
+
 socket.on('connect', function () {
     console.log('Connected to server');
 
@@ -15,6 +31,7 @@ socket.on('newMessage', function (message)  {
     });
 
     jQuery('#messages').append(html);
+    scrollToBottom();
 });
 
 socket.on('disconnect', function ()  {
@@ -31,6 +48,7 @@ socket.on('newLocationMessage', function(message) {
     });
 
     jQuery('#messages').append(html);
+    scrollToBottom();
 });
 
 
